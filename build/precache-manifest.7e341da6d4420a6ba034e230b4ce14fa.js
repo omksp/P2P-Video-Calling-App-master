7cd1586034545d7e72902639e4942641d4394d0c
omksp <28omkarpawar@gmail.com>
2021-11-13 23:56:24 +0530

    first commit

---
+++ build/precache-manifest.7e341da6d4420a6ba034e230b4ce14fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f15540ceab623034d1a074751bef1da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d59acfb503f81b11303",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "86ecea9cc397331c349c",
+    "url": "/static/js/2.4e7617e7.chunk.js"
+  },
+  {
+    "revision": "4d59acfb503f81b11303",
+    "url": "/static/js/main.4955a32b.chunk.js"
+  },
+  {
+    "revision": "1893be08da971e1fc9d5",
+    "url": "/static/js/runtime-main.913038c8.js"
+  },
+  {
+    "revision": "56248c525e17bc05c65c275290c49385",
+    "url": "/static/media/video-bg.56248c52.jpg"
+  }
+]);
